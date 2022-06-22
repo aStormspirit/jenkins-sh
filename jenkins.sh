3ed257901b3e1beb6107f1dd4fcab2e1429c5611
aStormSpirit <jedaiok2@mail.ru>
2022-06-22 10:27:13 +0300

    fix

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-apt update
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt update
+sudo apt update
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get install openjdk-11-jdk
+sudo apt update
 sudo apt install jenkins
-systemctl start jenkins
+sudo systemctl start jenkins
 echo "copy your root password"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 echo "end"
